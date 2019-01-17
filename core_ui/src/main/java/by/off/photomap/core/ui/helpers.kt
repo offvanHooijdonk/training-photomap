ec05eecea8c58ae6bc50697dac59fcedcb0c1170
offvanhooijdonk <offspring25@gmail.com>
2019-01-17 17:43:40 +0300

    Added Splash screen enhancements

---
+++ core_ui/src/main/java/by/off/photomap/core/ui/helpers.kt
@@ -0,0 +1,15 @@
+package by.off.photomap.core.ui
+
+import android.view.View
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
