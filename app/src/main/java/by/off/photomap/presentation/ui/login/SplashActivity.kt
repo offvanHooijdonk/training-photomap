2f284f38b417343aa092c7f97ef13e726ee6f073
offvanhooijdonk <offspring25@gmail.com>
2019-01-17 16:17:11 +0300

    Initial (almost) commit

---
+++ app/src/main/java/by/off/photomap/presentation/ui/login/SplashActivity.kt
@@ -0,0 +1,14 @@
+package by.off.photomap.presentation.ui.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import by.off.photomap.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.screen_splash)
+    }
+}
